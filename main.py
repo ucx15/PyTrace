83c53148b06ba6446af232b9f3270d0c67ccfaed
Udit Chandra <83822686+udit-uc828@users.noreply.github.com>
2021-05-08 16:34:49 +0530

    Update main.py
    Removed Output/ Directory from file output path

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from rt_lib import *
+from rt_lib import render_loop
 from Spheres import *
 from PIL import Image
 from time import time
@@ -20,6 +20,6 @@ t2 = time()
 total_t = t2-t1
 
 print(total_t)
-file_name = "Output/"+ scene_data.f_name +str(total_t)
+file_name = f"{scene_data.f_name} {total_t}"
 out_frmt = "png"
-img.save(file_name+"."+out_frmt)
+img.save(f"{file_name}.{out_frmt}")
