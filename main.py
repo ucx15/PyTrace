77d57b36ed903c0c7e5c8f304f04bba15a3f3b4c
Udit Chandra <83822686+udit-uc828@users.noreply.github.com>
2021-05-09 19:39:29 +0530

    Update main.py
    Now files are saved to Output folder not directly into main directory

--- main.py
+++ main.py
@@ -20,6 +20,6 @@ t2 = time()
 total_t = t2-t1
 
 print(total_t)
-file_name = f"{scene_data.f_name} {total_t}"
+file_name = f"Output/{scene_data.f_name} {total_t}"
 out_frmt = "png"
 img.save(f"{file_name}.{out_frmt}")
