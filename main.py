83efdec9bd79ac3b59015915c46231378f78823f
Udit Chandra <83822686+udit-uc828@users.noreply.github.com>
2021-05-10 09:51:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-from rt_lib import render_loop
-from Scene_files.Spheres import *
+from Scene_files.All_features_Scene import *
 from PIL import Image
 from time import time
 
 
-
-####__==__RT_LIBRARY___==__####
-
-####_____END_of_LIB_____####
-
-
 ####_____MAIN____####
 
-img = Image.new("RGB", (W,H), color=scene_data.bkg.to_rgb_q(8, 1, scene_data.gamma))
+img = Image.new("RGB", (W,H))
 
 t1 = time()
 img = render_loop(scene_data, img)
@@ -20,6 +13,6 @@ t2 = time()
 total_t = t2-t1
 
 print(total_t)
-file_name = f"Output/{scene_data.f_name} {total_t}"
+file_name = f"Output/ {scene_data.f_name} {total_t}"
 out_frmt = "png"
 img.save(f"{file_name}.{out_frmt}")
