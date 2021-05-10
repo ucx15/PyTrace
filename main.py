7794fa3e504ded3f87fd0218e0c921370a2e8196
Udit Chandra <83822686+udit-uc828@users.noreply.github.com>
2021-05-10 16:05:17 +0530

    Update main.py
    Refactor and optimize the code

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-from Scene_files.All_features_Scene import *
 from PIL import Image
 from time import time
 
+from Scene_files.All_features_Scene import *
 
-####_____MAIN____####
 
-img = Image.new("RGB", (W,H))
+if __name__ == "__main__":
 
-t1 = time()
-img = render_loop(scene_data, img)
-t2 = time()
-total_t = t2-t1
+	img = Image.new("RGB", (W,H))
 
-print(total_t)
-file_name = f"Output/ {scene_data.f_name} {total_t}"
-out_frmt = "png"
-img.save(f"{file_name}.{out_frmt}")
+	t1 = time()
+	img = render_loop(scene, img)
+	t2 = time()
+	
+	TotalTime_s = t2-t1
+	TotalTime_m = Minutes(TotalTime_s)
+	print(f"{TotalTime_s} \n {TotalTime_m}")
+
+	file_name = f"Output/ {scene.f_name} {TotalTime_s}"
+	out_frmt = "png"
+
+	img.save(f"{file_name}.{out_frmt}")
