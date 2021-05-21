140ac0978ad83092308e8a4eaa08b0c3c8135141
Udit Chandra <83822686+udit-uc828@users.noreply.github.com>
2021-05-22 03:15:32 +0530

    Refactor Code

--- Scene_files/All_features_Scene.py
+++ Scene_files/All_features_Scene.py
@@ -9,34 +9,26 @@ BR = 2        #radius
 Sp1 = Sphere(Vec(-4.1,0,0), BR)
 Sp2 = Sphere(Vec(0,0,0), BR)
 Sp3 = Sphere(Vec(4.1,0,0), BR)
-Sp4 = Sphere(Vec(0,0,-BR-99999), 99999) #Plane obj not worked; used sphere as a plane
-
+P = Plane(Vec(0, 0, -BR), Vec(0, 0, 1))
 
 #set_materials
-red = Material(Color(1, 0,0))
-metal = Material()
-white = Material()
+red = Material(Color(1, 0,0), rough=0)
+metal = Material(rough=0,type="GLOSS")
+white = Material(Color(.05,.05,.05),rough=0)
+
 blue = Material(Color(0,.07, 1))
-
-white.roughness = metal.roughness = 0
-red.roughness = 0
-
-
-white.type = red.type = "DIFF + GLOSS"
-metal.type = "GLOSS"
-
 
 Sp1.material = red
 Sp2.material = metal
 Sp3.material = blue
-Sp4.material = white
+P.material = white
 
-obj_lst = [ Sp1,Sp2, Sp3, Sp4]
+obj_lst = [Sp1, Sp2, Sp3, P]
 
 
 #_LIGHTS
-L1 = Light(Vec(-20, -20, 20), 12000)     #Key
-L2 = Light(Vec(0,-20,1), 150) #Fill
+L1 = Light(Vec(-20, -20, 20), 25000)     #Key
+L2 = Light(Vec(0,-20,1), 200) #Fill
 L1.shadows = 1
 
 light_lst = [L1, L2]
@@ -45,7 +37,7 @@ light_lst = [L1, L2]
 c_loc = Vec(0, -10, 3)
 c_at = Sp2.loc
 c_up = Vec(0,0,1)
-c_fov = 22
+c_fov = 24
 
 cam = Camera(c_loc,c_at,c_up,c_fov,W,H)
 
@@ -56,4 +48,5 @@ scene = Scene(obj_lst, cam, light_lst, W,H)
 
 scene.reflections = True
 scene.samples = 32 #reflection samples (1-128), can be >128 @cost of speed
-scene.depth = 1   #reflection depth (0-2), can be >2 @cost of speed
+scene.depth = 2   #reflection depth (0-2), can be >2 @cost of speed
+scene.exposure = 2
